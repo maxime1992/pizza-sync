0460d8091d8e292ce75228c6ee68abc0bd0cdab9
Maxime Robert <maxime.robert1992@gmail.com>
2017-01-26 00:01:21 +0100

    chore: Setup/conf cors with express

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const { getPizzas } = require('./pizzas-api')
+
+app.use(cors())
 
 app.get('/pizzas', (req, res) => {
   getPizzas().then(pizzas => res.json(pizzas))
