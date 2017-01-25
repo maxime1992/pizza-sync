60242d0082145515e2fdc507a6e6e2d699208916
Maxime Robert <maxime.robert1992@gmail.com>
2017-01-25 22:07:44 +0100

    feat: Create a pizza API to parse website and return normalized object

---
+++ backend/pizzas-api.js
@@ -0,0 +1,86 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+const getPizzas = () => {
+  return new Promise((resolve) => {
+    // keep track of the IDs
+    let ids = {
+      _pizzaId: 0,
+      get pizzaId() {
+        return `pizzaId${this._pizzaId++}`
+      },
+
+      _pizzaCategoryId: 0,
+      get pizzaCategoryId() {
+        return `pizzaCategoryId${this._pizzaCategoryId++}`
+      },
+
+      _personId: 0,
+      get personId() {
+        return `personId${this._personId++}`
+      }
+    }
+
+    // fetch the website
+    request('http://www.pizzadelormeau.com/nos-pizzas/', (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        // build the response object containing the pizzas and pizzas categories
+        const res = {
+          pizzas: { byId: {}, allIds: [] },
+          pizzasCategories: { byId: {}, allIds: [] },
+          people: { byId: {}, allIds: [] }
+        }
+
+        const $ = cheerio.load(body)
+
+        const sectionsDom = $('.entry-content .section')
+
+        sectionsDom.map(i => {
+          sectionDom = $(sectionsDom[i])
+
+          const pizzaCategory = sectionDom.find($('.title')).children().remove().end().text()
+
+          const finalPizzaCategory = {
+            id: ids.pizzaCategoryId,
+            name: pizzaCategory
+          }
+
+          res.pizzasCategories.byId[finalPizzaCategory.id] = finalPizzaCategory
+          res.pizzasCategories.allIds.push(finalPizzaCategory.id)
+
+          const pizzasDom = sectionDom.find($('.corps'))
+
+          pizzasDom.map(j => {
+            const pizzaDom = $(pizzasDom[j])
+
+            const pizzaName = pizzaDom.find($('.nom')).children().remove().end().text()
+            const pizzaIngredients = pizzaDom.find($('.composition')).text()
+            const pizzaPrixDom = pizzaDom.find($('.prix'))
+
+            const pizzaPrix = []
+            pizzaPrixDom.map(k => {
+              const prix = $(pizzaPrixDom[k]).children().remove().end().text()
+              pizzaPrix.push(parseFloat(prix))
+            })
+
+            const finalPizza = {
+              id: ids.pizzaId,
+              name: pizzaName,
+              ingredients: pizzaIngredients,
+              prix: pizzaPrix,
+              category: finalPizzaCategory.id
+            }
+
+            res.pizzas.byId[finalPizza.id] = finalPizza
+            res.pizzas.allIds.push(finalPizza.id)
+          })
+        })
+
+        resolve(res);
+      }
+    })
+  })
+}
+
+// getPizzas().then(res => console.log(JSON.stringify(res, null, 2)))
+        
