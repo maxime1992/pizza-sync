887ce4d51e52be2ed53b5a2373629caea57c9371
Maxime Robert <maxime.robert1992@gmail.com>
2017-11-16 19:58:46 +0100

    refactor(backend): remove old backend folder to start on a fresh (nestjs) base

--- backend/helpers/http.helper.js
+++
@@ -1,7 +0,0 @@
-const requestOptions = {
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/55.0.2883.87 Chrome/55.0.2883.87 Safari/537.36'
-  }
-}
-
-module.exports = { requestOptions }
